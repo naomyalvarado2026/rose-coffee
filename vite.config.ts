9455c719ecc77ce3886314789c1c9b6e6ad94ad7
Naomy Alvarado <naomyalvarado2026@users.noreply.github.com>
2026-06-29 03:19:59 -0500

    Set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/rose-coffee/',
   plugins: [
     {
       name: 'configure-response-headers',
